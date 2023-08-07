9d2fe8ce9cd5054fbb10a1b6f41a4689e8218d8f
Javier Ramirez Cospin <ram18099@uvg.edu.gt>
2023-08-07 01:49:10 -0600

    - Changed name of convertInput function in Receptor.java to SetTrama, to reset trama when needed - Added fixTrama function in Receptor.java for Hamming 7 4 Algorithm (function overload with no arguments) - Added parse74Block function in Receptor.java to obtain parity in a 7 bit block - Added binToString function in Receptor.java to convert an Integer List to a String - Added Comment lines above each function in Receptor.java for readability

--- src/main/java/lib/Receptor.java
+++ src/main/java/lib/Receptor.java
@@ -1,20 +1,16 @@
 package lib;
 
-import java.sql.Array;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Receptor {
     int bit_len;
     List<String> rec_trama;
-    List<Integer> og_trama;
-    List<Integer> poly;
+    List<Integer> og_trama, poly;
     String err_msg;
-    int RECURSION_LIMIT;
+    boolean full_mode;
 
-    public Receptor(String trama) {
-        this.rec_trama = Arrays.asList(trama.split(""));
-        this.RECURSION_LIMIT = 100;
+    public Receptor(boolean full_mode) {
         this.err_msg = "";
         /* IEEE 802 CRC-32 */
         this.poly = Arrays.asList(1, 0, 0, 0, 0, 0, 1, 1, 1);
@@ -23,34 +19,38 @@ public class Receptor {
                                     1, 0, 0, 0, 1, 1, 1, 0,
                                     1, 1, 0, 1, 1, 0, 1, 1, 1);*/
         this.bit_len = this.poly.size()-1;
+        this.full_mode = full_mode;
     }
 
-    public boolean convertInput() {
+    // Function to reset the current trama with (trama) provided
+    public boolean setTrama(String trama) {
+        this.rec_trama = Arrays.asList(trama.split(""));
         try {
             this.og_trama = this.rec_trama
                     .stream()
                     .map(Integer::parseInt)
                     .collect(Collectors.toList());
-            if(this.og_trama.size() < this.poly.size()) {
+            if(this.og_trama.size() < this.poly.size() && this.full_mode) {
                 this.err_msg = "Original trama is shorter than polynomial length ...";
                 return false;
             }
-            return true;
         } catch(Exception e) {
             this.err_msg = "Element in received trama or parity is not of type Integer ...";
             return false;
         }
+        return true;
     }
 
+    // Implementation of CRC algorithm for a (og_trama) trama & with (poly) polynomial
     public int checkTrama() {
-        // CRC-32
+        int r_limit = 100;
         int indx = 0, offset = this.poly.size(), counter = 0;
         List<Integer> temp = new ArrayList<>();
-        boolean flag = true;
+        boolean flag;
         while(indx < this.og_trama.size()) {
             flag = indx == 0;
             counter = offset == 0 ? 0 : counter + 1;
-            if(counter == this.RECURSION_LIMIT) break;
+            if(counter == r_limit) break;
             for(int x = 0; x < offset; x++) {
                 temp.add(this.og_trama.get(indx));
                 indx++;
@@ -69,11 +69,10 @@ public class Receptor {
                     offset = this.og_trama.size() - indx;
                 }
             }
-
         }
         temp = new ArrayList<>(temp.subList(temp.size()-this.bit_len, temp.size()));
-        if ( counter == this.RECURSION_LIMIT) {
-            this.err_msg = "Max recursion reached ("+this.RECURSION_LIMIT+")";
+        if ( counter == r_limit) {
+            this.err_msg = "Max recursion reached ("+r_limit+")";
             return -1;
         }
         if(temp.contains(1)) {
@@ -81,10 +80,10 @@ public class Receptor {
             return 0;
         }
         this.og_trama = new ArrayList<>(this.og_trama.subList(0, this.og_trama.size() - this.bit_len));
-        this.og_trama.addAll(temp);
         return 1;
     }
 
+    // Function for XOR operation between (list1) && (og_trama) || (list1) && [0, 0, 0, ... 0]
     public void xor(List<Integer> list1, boolean flag) {
         if (list1.get(0) == 1 || flag) {
             for (int i = 0; i < list1.size(); i++) {
@@ -98,8 +97,9 @@ public class Receptor {
         }
     }
 
+    // Implementation of Hamming Error Correction Algorithm, with
+    // 2^(parity.size()) >= (parity.size()) + (og_trama.size()) + 1
     public int fixTrama(String parity) {
-        // HAMMING ALGORITHM
         List<Integer> hammng = Arrays.stream(parity.split(""))
                 .map(Integer::parseInt)
                 .toList();
@@ -141,6 +141,7 @@ public class Receptor {
         return 1;
     }
 
+    // Function to generate a truth table up to 2^(rows) & with (max) entries each
     public List<List<Integer>> genTruthTable(int rows, int max) {
         List<List<Integer>> list = new ArrayList<>();
         for(int i = 0; i < rows; i++) {
@@ -158,6 +159,55 @@ public class Receptor {
         return list;
     }
 
+    // Implementation of Hamming 7 4 Error Correction Algorithm for a MOD(7) list
+    public int fixTrama() {
+        List<Integer> temp = new ArrayList<>();
+        if(this.og_trama.size() % 7 != 0) {
+            int spaces = ((int)Math.ceil((double)this.og_trama.size()/7)*7) - this.og_trama.size();
+            temp = Collections.nCopies(spaces, 0);
+            temp.addAll(this.og_trama);
+            this.og_trama = temp;
+        }
+        int indx = 0, pad = 7;
+        List<Integer> result = new ArrayList<>();
+        boolean flag = false;
+        while(indx < this.og_trama.size()) {
+            List<Integer> block = new ArrayList<>(this.og_trama.subList(indx, indx+pad));
+            temp = parse74Block(block);
+            int bit_pos = binToDec(temp);
+            if(binToDec(temp) == 0) {
+                result.addAll(Arrays.asList(block.get(0), block.get(1),
+                        block.get(2), block.get(4)));
+            } else {
+                for(int i = 0; i < 5; i++) {
+                    block.set(7 - bit_pos, block.get(7 - bit_pos) == 0 ? 1 : 0);
+                    temp = parse74Block(block);
+                    bit_pos = binToDec(temp);
+                    if(bit_pos == 0) {
+                        result.addAll(Arrays.asList(block.get(0), block.get(1),
+                                block.get(2), block.get(4)));
+                        break;
+                    }
+                    if(i == 4) flag = true;
+                }
+                if(flag) break;
+            }
+            indx += pad;
+        }
+        if(flag) return 0;
+        this.og_trama = result;
+        return 1;
+    }
+
+    // Function to calculate bit parity in a block on the Hamming 7 4 Algorithm
+    public List<Integer> parse74Block(List<Integer> block) {
+        int p1 = (block.get(0)+block.get(2)+block.get(4)+block.get(6)) % 2;
+        int p2 = (block.get(0)+block.get(1)+block.get(4)+block.get(5)) % 2;
+        int p4 = (block.get(0)+block.get(1)+block.get(2)+block.get(3)) % 2;
+        return new ArrayList<>(Arrays.asList(p1, p2, p4));
+    }
+
+    // Function used to convert an Integer array [0, 1] into a decimal number
     public int binToDec(List<Integer> list) {
         int result = 0;
         for(int i = 0; i < list.size(); i++) {
@@ -166,10 +216,36 @@ public class Receptor {
         return result;
     }
 
+    // Function to convert a trama into a String
+    public String binToString() {
+        List<Integer> temp = new ArrayList<>();
+        if(this.og_trama.size() % 8 != 0) {
+            int spaces = ((int)Math.ceil((double)this.og_trama.size()/8)) - this.og_trama.size();
+            temp = Collections.nCopies(spaces, 0);
+            temp.addAll(this.og_trama);
+            this.og_trama = temp;
+        }
+        String result = "";
+        int indx = 0, pad = 8;
+        while(indx < this.og_trama.size()) {
+            temp.clear();
+            temp = new ArrayList<>(this.og_trama.subList(indx, indx+pad));
+            String block = "";
+            for(int x = 0; x < 8; x++) {
+                block += temp.get(x);
+            }
+            result += (char)Integer.parseInt(block, 2);
+            indx += pad;
+        }
+        return result;
+    }
+
+    // Show error produced during one of the validations
     public void printError() {
         System.out.println("> Error: "+this.err_msg);
     }
 
+    // Show trama received and final trama
     public void printTramas() {
         System.out.println("\nReceived trama: "+this.rec_trama);
         System.out.println("Converted trama: "+this.og_trama);
